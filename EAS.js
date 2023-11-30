c244b15fbd1afad8d5fdb6caeea5038db2492622
jsarles-code <57759984+jsarles-code@users.noreply.github.com>
2023-11-30 15:37:58 -0800

    Update EAS.js

--- EAS.js
+++ EAS.js
@@ -1,13 +1,14 @@
 
-function createGrid(){
+// create a grid of divs with a user defined size
+function createGrid(size){
     let grid = document.createElement('div');
     grid.className = 'eas-grid';
     document.body.appendChild(grid);
-    for (let i = 0; ; i++) {
+    for (let i = 0; i < size; i++) {
       let row = document.createElement('div');
       row.className = 'row';
       grid.appendChild(row);
-      for (let j = 0; ; j++) {
+      for (let j = 0; j < size; j++) {
         let cell = document.createElement('div');
         cell.className = 'cell';
         row.appendChild(cell);
@@ -15,7 +16,8 @@ function createGrid(){
     }
     
 }
-createGrid();
+createGrid(16);
+
 //Use flexbox to make the divs appear as a grid
 //* /Create a function that will change the color of a div when the mouse hovers over it
 /* function changeColor(){
