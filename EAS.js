ce465c08429b5ac929826cf977cdac4b8abc1dc4
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-11-30 13:11:43 -0800

    Update EAS.js

--- EAS.js
+++ EAS.js
@@ -16,4 +16,14 @@ function createGrid(){
     }
     
 }
-createGrid();
+createGrid(16,16);
+//Use flexbox to make the divs appear as a grid
+//Create a function that will change the color of a div when the mouse hovers over it
+function changeColor(){
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = 'black';
+        });
+    });
+}
