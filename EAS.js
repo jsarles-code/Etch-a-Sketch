a1a8dba5cef3b332a060920ed6cac20ee9a50962
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-12-02 14:38:00 -0800

    Update EAS.js
    Refactor createGrid function to dynamically create
    a grid of divs based on user input

--- EAS.js
+++ EAS.js
@@ -1,30 +1,18 @@
-
-function createGrid(){
-    let grid = document.createElement('div');
-    grid.className = 'eas-grid';
-    document.body.appendChild(grid);
-    for (let i = 0; ; i++) {
-      let row = document.createElement('div');
-      row.className = 'row';
-      grid.appendChild(row);
-      for (let j = 0; ; j++) {
-        let cell = document.createElement('div');
-        cell.className = 'cell';
-        row.appendChild(cell);
-      }
+//function that creates a grid of divs the size of the input
+function createGrid()
+{
+  var grid = document.getElementById("grid");
+  var size = prompt("Enter a number between 1 and 100");
+  var width = 100 / size;
+  var height = 100 / size;
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      var div = document.createElement("div");
+      div.style.width = width + "%";
+      div.style.height = height + "%";
+      div.style.float = "left";
+      div.style.border = "1px solid black";
     }
-
-    
+  }
 }
 createGrid();
-//Use flexbox to make the divs appear as a grid
-//* /Create a function that will change the color of a div when the mouse hovers over it
-/* function changeColor(){
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-        cell.addEventListener('mouseover', function(e){
-            e.target.style.backgroundColor = 'black';
-        });
-    });
-}
-changeColor(); */ 
