0ed9448d57c3698e4027a8fcaa48ff2f4bf3af29
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-12-02 14:44:53 -0800

    Add grid creation function and hover effect
    Add grid creation function and hover effect

--- EAS.js
+++ EAS.js
@@ -1,4 +1,8 @@
-//function that creates a grid of divs the size of the input
+//function that creates a grid of divs the size of the input using flexbox
+
+// Add a button to the top of the screen which will clear the current grid and send the user a popup asking for the number of squares per side for the new grid. Once entered the new grid should be generated in the same total space as before (e.g. 960px wide) and now you’ve got a new sketch pad.
+// (Optional): Instead of just changing the color of your grid from black to white (for example) have each pass through it with the mouse change to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
+// Push your project to GitHub!
 function createGrid()
 {
   var grid = document.getElementById("grid");
@@ -12,6 +16,10 @@ function createGrid()
       div.style.height = height + "%";
       div.style.float = "left";
       div.style.border = "1px solid black";
+      // Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+      div.addEventListener("mouseover", function() {
+        this.style.backgroundColor = "black";
+      });
     }
   }
 }
