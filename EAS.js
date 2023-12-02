5b9d10ce3922fdf56108e423246edeba688b830a
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-12-02 15:11:31 -0800

    Add event listener for reset button
    Add event listener for reset button

--- EAS.js
+++ EAS.js
@@ -22,6 +22,7 @@ function createGrid() {
 
 // add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad.
 let resetButton = document.querySelector('#reset');
+resetButton.addEventListener("click", resizeGrid);
 function resizeGrid() {
     let container = document.querySelector('#container');
     while (container.firstChild) {
