6d7bfe81b63c3a9a1127d365c2a95bb0febea125
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-12-04 16:40:53 -0800

    add border and set max width

--- EAS.js
+++ EAS.js
@@ -3,11 +3,14 @@
 // Add a button to the top of the screen which will clear the current grid and send the user a popup asking for the number of squares per side for the new grid. Once entered the new grid should be generated in the same total space as before (e.g. 960px wide) and now you’ve got a new sketch pad.
 //function that creates a grid of square divs the size of the input using flexbox inside a container div that already exists in the HTML
 function createGrid() {
-  // set max width of container to 960px
-  container.style.maxWidth = "960px";
+  
   let container = document.querySelector('#container');
   let squaresPerSide = prompt("How many squares per side would you like?");
   let squareSize = (960 / squaresPerSide) + "px";
+  // set max width of container to 960px
+  container.style.maxWidth = "960px";
+  // add border to sketchpad
+  container.style.border = "2px solid black";
   for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
